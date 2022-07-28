e2693107659b331b2674ec1df33e19470e17f728
Roger Qiu <roger.qiu@polyhack.io>
2022-07-29 02:59:49 +1000

    feat: initial docs deployment to `polykey.io/docs`

--- docs/Home.md
+++ docs/Home.md
@@ -28,7 +28,9 @@ vaults with other users of PolyKey who are running their own keynodes.
 
 The wiki is structured using Divio system: https://documentation.divio.com/
 
-<img src="/images/divio_quadrant.png" width="70%" />
+![](/images/divio_quadrant.png)
+
+<!-- <img src="/images/divio_quadrant.png" width="70%" /> -->
 
 * Tutorials - these are a series of steps to introduce Polykey to new beginners to achieve a practical outcome
 * How-To Guides - these are short guides on how to achieve a specific use-case which makes assumptions on the reader
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,73 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { getAssetFromKV, NotFoundError, MethodNotAllowedError, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+addEventListener('fetch', (event) => {
+  event.respondWith(handleEvent(event))
+});
+
+async function handleEvent(event): Promise<Response> {
+  console.log('HANDLING EVENT');
+  console.log('event', event);
+  console.log('event.request', event.request);
+  console.log('event.request.url', event.request.url);
+  // console.log('event.request.cf', event.request.cf);
+
+  // The URL is `https://polykey.io/docs/...`, it is the full URL
+  // Use the `handlePrefix` to strip the `/docs/` out of the request
+  // The end result is that we have a URL that doesn't have the route in it
+  // So if we have `https://polykey.io/docs/index.html`
+  // This is converted to `https://polykey.io/index.html`
+  // The reason to do this is so that `mapRequestToAsset` will then use the `index.html` to look up
+  // Rather than the prefixed path
+  // The `getAssetFromKV` ignores the hostname, and just uses the path directly
+  // Which in this case will be index.html
+
+  const stripPrefix = handlePrefix(/^\/docs/);
+
+  try {
+    return await getAssetFromKV(event, {
+      mapRequestToAsset: (req) => {
+        const r = stripPrefix(req);
+        console.log('STRIPPED', r.url);
+        return r;
+      }
+    });
+  } catch (e) {
+    if (e instanceof NotFoundError) {
+      console.log('NOT FOUND', e.message);
+      const response404 = await getAssetFromKV(
+        event,
+        {
+          mapRequestToAsset: (req) =>  {
+            const url = new URL(req.url);
+            console.log('MAPPING TO', `${url.origin}/404.html`)
+            return new Request(`${url.origin}/404.html`, req);
+          },
+        }
+      );
+
+      console.log('GOT 404 response');
+
+      return new Response(response404.body, {
+        ...response404,
+        status: 404,
+      });
+    } else if (e instanceof MethodNotAllowedError) {
+      return new Response('Method Not Allowed', { status: 405 });
+    }
+    return new Response('Server Error', { status: 500 });
+  }
 }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+function handlePrefix(prefix) {
+  return (request) => {
+    // compute the default (e.g. / -> index.html)
+    let defaultAssetKey = mapRequestToAsset(request);
+    let url = new URL(defaultAssetKey.url);
+
+    // strip the prefix from the path for lookup
+    url.pathname = url.pathname.replace(prefix, "/");
+
+    // inherit all other props from the default request
+    return new Request(url.toString(), defaultAssetKey);
+  };
+}
