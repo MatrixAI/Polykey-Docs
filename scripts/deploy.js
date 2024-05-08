20b7957ddce5436a04c7a3931f3f5e2e787c99bc
Roger Qiu <roger.qiu@polyhack.io>
2024-05-08 19:59:55 +1000

    ci: added the custom deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+const os = require('os');
+const fs = require('fs');
+const process = require('process');
+const childProcess = require('child_process');
+const TOML = require('@iarna/toml');
+
+const platform = os.platform();
+
+/* eslint-disable no-console */
+async function main(argv = process.argv) {
+  argv = argv.slice(2);
+  // Optional feature is used for feature branches
+  let feature;
+  const restArgs = [];
+  while (argv.length > 0) {
+    const option = argv.shift();
+    let match;
+    if ((match = option.match(/--feature(?:=(.+)|$)/))) {
+      feature = match[1] ?? argv.shift();
+    } else {
+      restArgs.push(option);
+    }
+  }
+  // The `--feature X` option only makes sense if you also use `--env X`.
+  if (typeof feature === 'string') {
+    const wranglerConfig = TOML.parse(
+      fs.readFileSync('./wrangler.toml', { encoding: 'utf-8' }),
+    );
+    // Backup the original wrangler.toml
+    fs.renameSync('./wrangler.toml', './wrangler.toml.bak');
+    // All the handlers for swapping back the original wrangler.toml
+    process.on('beforeExit', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('uncaughtException', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('unhandledRejection', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('SIGINT', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('SIGTERM', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('SIGQUIT', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    process.on('SIGHUP', () => {
+      fs.renameSync('./wrangler.toml.bak', './wrangler.toml');
+    });
+    // Add in the feature environment
+    wranglerConfig.env[feature] = {
+      name: `${wranglerConfig.name}-${feature}`,
+      routes: [
+        {
+          pattern: `${feature}.${wranglerConfig.routes[0].pattern}`,
+          custom_domain: true,
+        },
+      ],
+    };
+    fs.writeFileSync('./wrangler.toml', TOML.stringify(wranglerConfig), {
+      encoding: 'utf-8',
+    });
+  }
+  const secretBulkArgs = [
+    'secret:bulk',
+    '--config',
+    './wrangler.toml',
+    ...restArgs,
+  ];
+  console.error(['wrangler', ...secretBulkArgs].join(' '));
+  childProcess.execFileSync('wrangler', secretBulkArgs, {
+    input: JSON.stringify({}),
+  });
+  const deployArgs = ['deploy', '--config', './wrangler.toml', ...restArgs];
+  console.error(['wrangler', ...deployArgs].join(' '));
+  childProcess.execFileSync('wrangler', deployArgs, {
+    stdio: ['inherit', 'inherit', 'inherit'],
+    windowsHide: true,
+    encoding: 'utf-8',
+    shell: platform === 'win32' ? true : false,
+  });
+}
+/* eslint-enable no-console */
+
+void main();
