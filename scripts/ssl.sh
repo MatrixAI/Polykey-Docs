4680db13fe20eaa62d1cc6f530955a8e64ec7c27
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-07-04 12:23:54 +1000

    ci: delete SSL certs upon PR close

---
+++ scripts/ssl.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+response=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/ssl/certificate_packs" \
+                -H "Content-Type: application/json" \
+                -H "X-Auth-Key: $AUTH_KEY" \
+                -H "X-Auth-Email: $AUTH_EMAIL")
+
+cert_ids=$(echo "$response" | jq -r --arg domain "$DOMAIN" '.result[] | select(.hosts[] | contains($domain)) | .id')
+
+echo "$cert_ids" | while read -r cert_id; do
+     curl -X DELETE --url "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/ssl/certificate_packs/$cert_id" \
+          -H "Content-Type: application/json" \
+          -H "X-Auth-Key: $AUTH_KEY" \
+          -H "X-Auth-Email: $AUTH_EMAIL"
+done
