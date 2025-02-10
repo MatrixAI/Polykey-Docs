65ecee694d4abff66b18dcbc81a6089fe4473500
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-02-11 08:06:20 +1100

    feat: update CI

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             shellHook = ''
               echo "Entering $(npm pkg get name)"
               set -o allexport
-              . <(pk secrets env Polykey-Docs:.)
+              . <(polykey secrets env Polykey-Docs)
               set +o allexport
               set -v
               ${lib.optionalString ci ''
